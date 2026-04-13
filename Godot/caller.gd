948ed6cdc8589deca83bfe168c3aca1269f638e0
rat ! <leahbreyergamedev@gmail.com>
2026-04-13 16:34:41 -0600

    call/recieve added godot

---
+++ Godot/caller.gd
@@ -0,0 +1,13 @@
+extends Node
+
+@export var receiver: Receiver
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("Hello Friend")
+	receiver.OnCalled()
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ Godot/receiver.gd
@@ -0,0 +1,7 @@
+extends Node
+
+class_name Receiver 
+func OnCalled() -> void:
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+	print("Hiiiiiiiiiiiiiiiiiiiiiiii, hello recieved")
+	
