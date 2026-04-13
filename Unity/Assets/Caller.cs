dc03b17a63a9248e62beb73adcf19d32d91178e7
rat ! <leahbreyergamedev@gmail.com>
2026-04-13 16:50:12 -0600

    Unity Object reciever done

---
+++ Unity/Assets/Caller.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Caller : MonoBehaviour
+{
+    public Receiver receiver;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello Friend");   
+        receiver.OnCalled();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
